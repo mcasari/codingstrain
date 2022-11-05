915634753000cdf4a69e17041345ca7860416166
mario casari <marioangelo.casari@eng.it>
2022-11-06 00:13:21 +0100

    refactoring

--- spring-boot-minimal-rest-h2/src/main/java/com/codingstrain/minimalrest/h2/MinimalRestH2App.java
+++ spring-boot-minimal-rest-h2/src/main/java/com/codingstrain/minimalrest/h2/MinimalRestH2App.java
@@ -3,8 +3,6 @@ package com.codingstrain.minimalrest.h2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
 public class MinimalRestH2App {
 
