9d6a3a7ffcfb57e1e0824cc11b292cd46b15c4d1
mcasari <mario.casari@gmail.com>
2023-01-06 19:48:14 +0100

    cloud config native client module added

--- spring-cloud-config-native-server/src/main/java/com/codingstrain/springcloud/config/server/ConfigNative.java
+++ spring-cloud-config-native-server/src/main/java/com/codingstrain/springcloud/config/server/ConfigNative.java
@@ -1,13 +1,16 @@
 package com.codingstrain.springcloud.config.server;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigNative {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigNative.class, args);
+		new SpringApplicationBuilder(ConfigNative.class).run(args);
 	}
 
 
