e1fd0903de993442c7c745ba14369932bb091a60
mcasari <mario.casari@gmail.com>
2023-01-06 18:55:43 +0100

    first commit

---
+++ spring-cloud-config-native/src/main/java/com/codingstrain/springcloud/config/ConfigNative.java
@@ -0,0 +1,14 @@
+package com.codingstrain.springcloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigNative {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigNative.class, args);
+	}
+
+
+}
---
+++ spring-cloud-config-native/src/test/java/com/codingstrain/springcloud/config/ConfigNativeTests.java
@@ -0,0 +1,13 @@
+package com.codingstrain.springcloud.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigNativeTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
