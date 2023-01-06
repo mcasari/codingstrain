47a9b7efbf7a08263a67b22dd0e216d899b0c7a9
mcasari <mario.casari@gmail.com>
2023-01-06 18:59:00 +0100

    refactoring

--- spring-cloud-config-native/src/main/java/com/codingstrain/springcloud/config/server/ConfigNative.java
+++ spring-cloud-config-native/src/main/java/com/codingstrain/springcloud/config/server/ConfigNative.java
@@ -1,4 +1,4 @@
-package com.codingstrain.springcloud.config;
+package com.codingstrain.springcloud.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-cloud-config-native/src/test/java/com/codingstrain/springcloud/config/server/ConfigNativeTests.java
+++ spring-cloud-config-native/src/test/java/com/codingstrain/springcloud/config/server/ConfigNativeTests.java
@@ -1,4 +1,4 @@
-package com.codingstrain.springcloud.config;
+package com.codingstrain.springcloud.config.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
