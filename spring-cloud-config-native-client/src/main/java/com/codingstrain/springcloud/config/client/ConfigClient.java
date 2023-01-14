fb7933a82e14199e014cea891721c6af07b3ec39
mcasari <mario.casari@gmail.com>
2023-01-15 00:42:08 +0100

    refactoring

--- spring-cloud-config-native-client/src/main/java/com/codingstrain/springcloud/config/client/ConfigClient.java
+++ spring-cloud-config-native-client/src/main/java/com/codingstrain/springcloud/config/client/ConfigClient.java
@@ -2,6 +2,7 @@ package com.codingstrain.springcloud.config.client;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
