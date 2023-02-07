25f035a21c69e118c720b88486ed316dc1d8fadf
mcasari <mario.casari@gmail.com>
2023-02-07 21:37:35 +0100

    added tree map sorting by comparator

---
+++ java-miscellaneous-tips/src/main/java/com/codingstrain/java/tips/TreeMapKeyComparatorSorting.java
@@ -0,0 +1,20 @@
+package com.codingstrain.java.tips;
+
+import java.util.*;
+
+class TreeMapKeyComparatorSorting {
+	 
+    public static void main(String args[]) {
+ 
+        TreeMap<String, String> treeMap = new TreeMap<>(Comparator.reverseOrder());
+        treeMap.put("z", "value1");
+        treeMap.put("c", "value2");
+        treeMap.put("a", "value3");
+        treeMap.put("b", "value4");
+ 
+        for (Map.Entry<String, String> mapEntry : treeMap.entrySet()) {
+            System.out.println(mapEntry.getKey() + " - " + mapEntry.getValue());
+        }
+    }
+ 
+}	
