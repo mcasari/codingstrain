dc05514cadea873791896ac4cda776a61722ade1
mcasari <mario.casari@gmail.com>
2023-04-06 17:37:00 +0200

    little advancement in configuration

--- spring-cloud-discovery-server/src/main/java/com/codingstrain/springcloud/discovery/server/AppMain.java
+++ spring-cloud-discovery-server/src/main/java/com/codingstrain/springcloud/discovery/server/AppMain.java
@@ -2,11 +2,11 @@ package com.codingstrain.springcloud.discovery.server;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableEurekaServer
 public class AppMain {
 
 	public static void main(String[] args) {
