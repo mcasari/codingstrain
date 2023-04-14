1c0124f5e7a1d27bc1e86a517a8c6d61baf4fb17
mcasari <mario.casari@gmail.com>
2023-04-14 20:44:03 +0200

    little advancement for discovery

--- spring-cloud-discovery-gateway-localconfig/src/main/java/com/codingstrain/springcloud/discovery/server/AppMain.java
+++ spring-cloud-discovery-gateway-localconfig/src/main/java/com/codingstrain/springcloud/discovery/server/AppMain.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class AppMain {
 
 	public static void main(String[] args) {
