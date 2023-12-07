b07446ddce4824b1ca615854b0ded85de36d46ee
macasari <macasari@MACASARIW.italy.itroot.adnet>
2023-12-07 11:16:23 +0100

    advancement

--- spring-cloud-sample-libraryapp/libraryapp-openfeign-discovery/libraryapp-discovery-books/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/AppMain.java
+++ spring-cloud-sample-libraryapp/libraryapp-openfeign-discovery/libraryapp-discovery-books/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/AppMain.java
@@ -2,12 +2,16 @@ package com.codingstrain.springcloud.sample.libraryapp.books;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class AppMain {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppMain.class, args);
+        SpringApplication.run(AppMain.class, args);
 	}
 
 
