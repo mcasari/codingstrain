3834d022298f4a8c0b34ce303dbd30a5471b423a
macasari <macasari@MACASARIW.italy.itroot.adnet>
2023-12-07 22:40:36 +0100

    advancement

--- spring-cloud-sample-libraryapp/libraryapp-openfeign-discovery/libraryapp-discovery-books/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/controller/BookController.java
+++ spring-cloud-sample-libraryapp/libraryapp-openfeign-discovery/libraryapp-discovery-books/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.codingstrain.springcloud.sample.libraryapp.books.controller;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.codingstrain.springcloud.sample.libraryapp.books.BookException;
+import com.codingstrain.springcloud.sample.libraryapp.books.client.AuthorClient;
+import com.codingstrain.springcloud.sample.libraryapp.books.client.ReviewClient;
 import com.codingstrain.springcloud.sample.libraryapp.books.dto.BookInfo;
+import com.codingstrain.springcloud.sample.libraryapp.books.model.Author;
 import com.codingstrain.springcloud.sample.libraryapp.books.model.Book;
+import com.codingstrain.springcloud.sample.libraryapp.books.model.Review;
 import com.codingstrain.springcloud.sample.libraryapp.books.service.BookService;
 
 @RestController
@@ -25,15 +30,26 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
+    @Autowired
+    private AuthorClient authorClient;
+
+    @Autowired
+    private ReviewClient reviewClient;
+
     @GetMapping(value = "/bookInfo", params = { "authorName", "bookTitle" })
     public BookInfo findBookInfoByTitle(@RequestParam("authorName") String authorName, @RequestParam("bookTitle") String bookTitle) {
-        try {
-            return bookService.findBookInfoByRestTemplate("http://localhost:8080/library/author", "http://localhost:8080/library/review", authorName,
-                bookTitle);
-        } catch (BookException e) {
-            logger.error("", e);
-        }
-        return null;
+        Optional<Author> author = authorClient.findByName(authorName);
+        List<Review> reviews = reviewClient.findByBookTitle(bookTitle);
+        BookInfo bookInfo = new BookInfo();
+        bookInfo.setAuthorBiography(author.get()
+            .getBiography());
+        bookInfo.setAuthorName(authorName);
+        List<String> reviewContents = reviews.stream()
+            .map(item -> item.getContent())
+            .collect(Collectors.toList());
+        bookInfo.setTitle(bookTitle);
+        bookInfo.setBookReviews(reviewContents);
+        return bookInfo;
     }
 
     @GetMapping(value = "/book/{title}")
--- spring-cloud-sample-libraryapp/libraryapp-openfeign-discovery/libraryapp-discovery-books/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/service/BookService.java
+++ spring-cloud-sample-libraryapp/libraryapp-openfeign-discovery/libraryapp-discovery-books/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/service/BookService.java
@@ -5,16 +5,10 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import com.codingstrain.springcloud.sample.libraryapp.books.BookException;
-import com.codingstrain.springcloud.sample.libraryapp.books.dto.BookInfo;
 import com.codingstrain.springcloud.sample.libraryapp.books.model.Book;
 import com.codingstrain.springcloud.sample.libraryapp.books.repository.BookRepository;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service("bookService")
 public class BookService {
@@ -24,57 +18,8 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
-    @Autowired
-    private RestTemplate restTemplate;
-
-    public BookInfo findBookInfoByRestTemplate(String urlAuthorInfo, String urlBookReviewInfo, String authorName, String bookTitle) throws BookException {
-        ResponseEntity<String> responseAuthorInfo = restTemplate.getForEntity(urlAuthorInfo + "/" + authorName, String.class);
-        BookInfo bookInfo = new BookInfo();
-        bookInfo = parseAuthorInfo(bookInfo, responseAuthorInfo);
-        ResponseEntity<String> response2 = restTemplate.getForEntity(urlBookReviewInfo + "/" + bookTitle, String.class);
-        bookInfo = parseReviewInfo(bookInfo, response2);
-        return bookInfo;
-    }
-
     public Optional<Book> findByTitle(String title) {
         return bookRepository.findById(title);
     }
 
-    private BookInfo parseAuthorInfo(BookInfo bookInfo, ResponseEntity<String> response) throws BookException {
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = null;
-        try {
-            root = mapper.readTree(response.getBody());
-            String authorName = "";
-            String authorBiography = "";
-            JsonNode node = root.path("name");
-            authorName = node != null ? node.asText() : "";
-            node = root.path("biography");
-            authorBiography = node != null ? node.asText() : "";
-            bookInfo.setAuthorName(authorName);
-            bookInfo.setAuthorBiography(authorBiography);
-        } catch (Exception e) {
-            throw new BookException("Error in parsing JSON author info!", e);
-        }
-        return null;
-    }
-
-    private BookInfo parseReviewInfo(BookInfo bookInfo, ResponseEntity<String> response) throws BookException {
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = null;
-        try {
-            root = mapper.readTree(response.getBody());
-            String authorName = "";
-            String authorBiography = "";
-            JsonNode node = root.path("name");
-            authorName = node != null ? node.asText() : "";
-            node = root.path("biography");
-            authorBiography = node != null ? node.asText() : "";
-            bookInfo.setAuthorName(authorName);
-            bookInfo.setAuthorBiography(authorBiography);
-        } catch (Exception e) {
-            throw new BookException("Error in parsing JSON reviews info!", e);
-        }
-        return null;
-    }
 }
