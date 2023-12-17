c553376dc059a7df54563797f344a2329acba5a3
macasari <macasari@MACASARIW.italy.itroot.adnet>
2023-12-17 23:46:39 +0100

    advancement

--- spring-cloud-sample-libraryapp/libraryapp-openfeign-discovery/libraryapp-discovery-books/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/controller/BookController.java
+++ spring-cloud-sample-libraryapp/libraryapp-openfeign-discovery/libraryapp-discovery-books/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/controller/BookController.java
@@ -1,8 +1,6 @@
 package com.codingstrain.springcloud.sample.libraryapp.books.controller;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,12 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.codingstrain.springcloud.sample.libraryapp.books.client.AuthorClient;
-import com.codingstrain.springcloud.sample.libraryapp.books.client.ReviewClient;
 import com.codingstrain.springcloud.sample.libraryapp.books.dto.BookInfo;
-import com.codingstrain.springcloud.sample.libraryapp.books.model.Author;
 import com.codingstrain.springcloud.sample.libraryapp.books.model.Book;
-import com.codingstrain.springcloud.sample.libraryapp.books.model.Review;
 import com.codingstrain.springcloud.sample.libraryapp.books.service.BookService;
 
 @RestController
@@ -30,26 +24,9 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
-    @Autowired
-    private AuthorClient authorClient;
-
-    @Autowired
-    private ReviewClient reviewClient;
-
     @GetMapping(value = "/bookInfo", params = { "authorName", "bookTitle" })
     public BookInfo findBookInfoByTitle(@RequestParam("authorName") String authorName, @RequestParam("bookTitle") String bookTitle) {
-        Optional<Author> author = authorClient.findByName(authorName);
-        List<Review> reviews = reviewClient.findByBookTitle(bookTitle);
-        BookInfo bookInfo = new BookInfo();
-        bookInfo.setAuthorBiography(author.get()
-            .getBiography());
-        bookInfo.setAuthorName(authorName);
-        List<String> reviewContents = reviews.stream()
-            .map(item -> item.getContent())
-            .collect(Collectors.toList());
-        bookInfo.setTitle(bookTitle);
-        bookInfo.setBookReviews(reviewContents);
-        return bookInfo;
+        return bookService.findBookInfoByTitle(authorName, bookTitle);
     }
 
     @GetMapping(value = "/book/{title}")
