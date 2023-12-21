2890f3f87811abb13ef53483cc80d4ed74ee40f9
macasari <macasari@MACASARIW.italy.itroot.adnet>
2023-12-21 23:53:17 +0100

    advancement

--- spring-cloud-sample-libraryapp/libraryapp-openfeign-discovery/libraryapp-discovery-books/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/client/AuthorClient.java
+++ spring-cloud-sample-libraryapp/libraryapp-openfeign-discovery/libraryapp-discovery-books/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/client/AuthorClient.java
@@ -1,18 +1,11 @@
 package com.codingstrain.springcloud.sample.libraryapp.books.client;
 
-import java.util.Optional;
-
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import com.codingstrain.springcloud.sample.libraryapp.authors.model.Author;
+import com.codingstrain.springcloud.sample.libraryapp.authors.rest.AuthorRESTService;
 
 
 @FeignClient(name = "author-service")
-public interface AuthorClient {
+public interface AuthorClient extends AuthorRESTService {
 	
-    @GetMapping("/author/{name}")
-    public Optional<Author> findByName(@PathVariable("name") String name);
-
 }
--- spring-cloud-sample-libraryapp/libraryapp-openfeign-discovery/libraryapp-discovery-books/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/client/ReviewClient.java
+++ spring-cloud-sample-libraryapp/libraryapp-openfeign-discovery/libraryapp-discovery-books/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/client/ReviewClient.java
@@ -1,18 +1,11 @@
 package com.codingstrain.springcloud.sample.libraryapp.books.client;
 
-import java.util.List;
-
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import com.codingstrain.springcloud.sample.libraryapp.review.model.Review;
+import com.codingstrain.springcloud.sample.libraryapp.review.rest.ReviewRESTService;
 
 
 @FeignClient(name = "review-service")
-public interface ReviewClient {
-
-    @GetMapping("/review/{bookTitle}")
-    public List<Review> findByBookTitle(@PathVariable("bookTitle") String bookTitle);
+public interface ReviewClient extends ReviewRESTService {
 	
 }
