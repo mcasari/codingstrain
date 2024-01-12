056825a9fa61e15a5c0442e9ec14be355e765d04
macasari <macasari@MACASARIW.italy.itroot.adnet>
2024-01-13 00:13:23 +0100

    advancement

--- spring-cloud-sample-libraryapp/libraryapp-discovery-gateway/libraryapp-discovery-gateway-scg/src/main/java/com/codingstrain/springcloud/sample/libraryapp/discoveryserver/AppMain.java
+++ spring-cloud-sample-libraryapp/libraryapp-discovery-gateway/libraryapp-discovery-gateway-scg/src/main/java/com/codingstrain/springcloud/sample/libraryapp/discoveryserver/AppMain.java
@@ -2,17 +2,27 @@ package com.codingstrain.springcloud.sample.libraryapp.discoveryserver;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
 
 
 
 @SpringBootApplication
-@EnableEurekaServer
 public class AppMain {
 
 	public static void main(String[] args) {
         new SpringApplicationBuilder(AppMain.class).run(args);
 	}
+
+    @Bean
+    public RouterFunction<ServerResponse> getRoute() {
+        return GatewayRouterFunctions.route()
+            .GET("/get", HandlerFunctions.http("https://httpbin.org"))
+            .build();
+    }
 
 
 }
