8dfeb62553de7848907eca771f0d99e0c1791ef4
macasari <macasari@MACASARIW.italy.itroot.adnet>
2024-01-17 22:03:33 +0100

    advancement

--- java-miscellaneous-tips/src/test/java/com/codingstrain/java/tips/ShallowDeepCopyUnitTest.java
+++ java-miscellaneous-tips/src/test/java/com/codingstrain/java/tips/ShallowDeepCopyUnitTest.java
@@ -9,23 +9,23 @@ import com.codingstrain.java.tips.deepshallowcopy.Main;
 
 class ShallowDeepCopyUnitTest {
 
-@Test
-public void givenAnObjectAndADependency_whenShallowClonedAndDepencyChanges_ThenDepencyHasNewValue() throws Exception {
-    Dependency dependency = new Dependency(13);
-    Main main = new Main(dependency);
-    Main shallowClone = (Main) main.clone();
-    dependency.setValue(17);
-    assertEquals(17, shallowClone.getDependency()
-        .getValue());
-}
+    @Test
+    public void givenAnObjectAndADependency_whenDeepClonedAndDepencyChanges_ThenDepencyKeepsOldValue() throws Exception {
+        Dependency dependency = new Dependency(13);
+        Main main = new Main(dependency);
+        Main deepClone = (Main) main.deepClone();
+        dependency.setValue(17);
+        assertEquals(13, deepClone.getDependency()
+            .getValue());
+    }
 
-@Test
-public void givenAnObjectAndADependency_whenDeepClonedAndDepencyChanges_ThenDepencyKeepsOldValue() throws Exception {
-    Dependency dependency = new Dependency(13);
-    Main main = new Main(dependency);
-    Main deepClone = (Main) main.deepClone();
-    dependency.setValue(17);
-    assertEquals(13, deepClone.getDependency()
-        .getValue());
-}
+    @Test
+    public void givenAnObjectAndADependency_whenShallowClonedAndDepencyChanges_ThenDepencyHasNewValue() throws Exception {
+        Dependency dependency = new Dependency(13);
+        Main main = new Main(dependency);
+        Main shallowClone = (Main) main.clone();
+        dependency.setValue(17);
+        assertEquals(17, shallowClone.getDependency()
+            .getValue());
+    }
 }
