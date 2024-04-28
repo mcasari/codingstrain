28b69fdee8bb8698eb4742c6b1cee9b747033f88
macasari <macasari@MACASARIW.mshome.net>
2024-04-28 19:23:25 +0200

    aouth2 sample

---
+++ spring-cloud-sample-libraryapp/libraryapp-discovery-oauth2/libraryapp-discovery-gateway-oauth2/src/main/java/com/codingstrain/springcloud/sample/libraryapp/gatewayserver/AppMain.java
@@ -0,0 +1,16 @@
+package com.codingstrain.springcloud.sample.libraryapp.gatewayserver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+
+
+@SpringBootApplication
+public class AppMain {
+
+	public static void main(String[] args) {
+        new SpringApplicationBuilder(AppMain.class).run(args);
+	}
+
+
+}
