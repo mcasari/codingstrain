ab7032a8521f4b7433c769b30212ff0cd519e8a2
macasari <macasari@MACASARIW.italy.itroot.adnet>
2024-05-13 01:07:08 +0200

    advancement

--- spring-cloud-sample-libraryapp/libraryapp-discovery-ssl/libraryapp-discovery-books-ssl/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/controller/BookController.java
+++ spring-cloud-sample-libraryapp/libraryapp-discovery-ssl/libraryapp-discovery-books-ssl/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/controller/BookController.java
@@ -1,17 +1,12 @@
 package com.codingstrain.springcloud.sample.libraryapp.books.controller;
 
-import java.util.Optional;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codingstrain.springcloud.sample.libraryapp.books.service.BookService;
-import com.codingstrain.springcloud.sample.libraryapp.model.entity.Author;
 
 @RestController
 @RequestMapping("/library")
@@ -19,12 +14,9 @@ public class BookController {
 
     Logger logger = LoggerFactory.getLogger(BookService.class);
 
-    @Autowired
-    private BookService bookService;
-
-    @GetMapping(value = "/authorInfo", params = { "authorName" })
-    public Optional<Author> getAuthor(@RequestParam("authorName") String authorName) {
-        return bookService.getAuthor(authorName);
+    @GetMapping(value = "/ping")
+    public String ping() {
+        return "Ping executed!";
     }
 
 
