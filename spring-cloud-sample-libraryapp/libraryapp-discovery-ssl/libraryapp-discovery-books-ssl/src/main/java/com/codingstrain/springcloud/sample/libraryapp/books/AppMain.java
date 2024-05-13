7aba00ff90a7c1fdf5b82120d7cdb23f02226a2c
macasari <macasari@MACASARIW.mshome.net>
2024-05-13 22:09:00 +0200

    advancement

--- spring-cloud-sample-libraryapp/libraryapp-discovery-ssl/libraryapp-discovery-books-ssl/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/AppMain.java
+++ spring-cloud-sample-libraryapp/libraryapp-discovery-ssl/libraryapp-discovery-books-ssl/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/AppMain.java
@@ -1,17 +1,60 @@
 package com.codingstrain.springcloud.sample.libraryapp.books;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
 public class AppMain {
 
-	public static void main(String[] args) {
-        SpringApplication.run(AppMain.class, args);
-	}
+    @Value("${eureka.client.tls.trust-store}")
+    private Resource trustStore;
 
+    @Value("${eureka.client.tls.trust-store-password}")
+    private String trustStorePassword;
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppMain.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, CertificateException, MalformedURLException,
+                                       IOException {
+
+        SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray())
+            .build();
+        SSLConnectionSocketFactory sslConFactory = new SSLConnectionSocketFactory(sslContext);
+        HttpClientConnectionManager cm = PoolingHttpClientConnectionManagerBuilder.create()
+            .setSSLSocketFactory(sslConFactory)
+                .build();
+        CloseableHttpClient httpClient = HttpClients.custom()
+            .setConnectionManager(cm)
+            .build();
+        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        return new RestTemplate(requestFactory);
+    }
 
 
 }
