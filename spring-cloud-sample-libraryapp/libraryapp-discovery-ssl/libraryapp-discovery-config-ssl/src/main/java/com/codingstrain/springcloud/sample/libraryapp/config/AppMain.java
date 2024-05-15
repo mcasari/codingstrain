ea8bba15b38670aea557fd68824f03fe3365d0cf
macasari <macasari@MACASARIW.italy.itroot.adnet>
2024-05-15 22:20:56 +0200

    advancement

--- spring-cloud-sample-libraryapp/libraryapp-discovery-ssl/libraryapp-discovery-config-ssl/src/main/java/com/codingstrain/springcloud/sample/libraryapp/config/AppMain.java
+++ spring-cloud-sample-libraryapp/libraryapp-discovery-ssl/libraryapp-discovery-config-ssl/src/main/java/com/codingstrain/springcloud/sample/libraryapp/config/AppMain.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
@@ -20,10 +21,11 @@ public class AppMain {
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     
-            http.authorizeHttpRequests(auth -> auth.anyRequest()
+            http.authorizeHttpRequests(auth -> auth.requestMatchers("/decrypt")
+                .denyAll()
+                .anyRequest()
                 .permitAll())
-                .csrf()
-                .disable();
+                .csrf(CsrfConfigurer::disable);
     
             return http.build();
         }
