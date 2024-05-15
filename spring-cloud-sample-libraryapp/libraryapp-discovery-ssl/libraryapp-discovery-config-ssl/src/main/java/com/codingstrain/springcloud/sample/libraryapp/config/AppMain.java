53f3851f610ad55ac15b9b4396f362266a189a8f
macasari <macasari@MACASARIW.italy.itroot.adnet>
2024-05-15 14:17:45 +0200

    advancement

--- spring-cloud-sample-libraryapp/libraryapp-discovery-ssl/libraryapp-discovery-config-ssl/src/main/java/com/codingstrain/springcloud/sample/libraryapp/config/AppMain.java
+++ spring-cloud-sample-libraryapp/libraryapp-discovery-ssl/libraryapp-discovery-config-ssl/src/main/java/com/codingstrain/springcloud/sample/libraryapp/config/AppMain.java
@@ -3,6 +3,9 @@ package com.codingstrain.springcloud.sample.libraryapp.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -14,17 +17,16 @@ public class AppMain {
     }
 
 
-    //    @Bean
-    //    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    //
-    //        http.authorizeHttpRequests(auth -> auth.requestMatchers("/decrypt")
-    //            .denyAll()
-    //            .requestMatchers("/encrypt")
-    //            .permitAll())
-    //            .httpBasic(withDefaults());
-    //
-    //        return http.build();
-    //    }
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    
+            http.authorizeHttpRequests(auth -> auth.anyRequest()
+                .permitAll())
+                .csrf()
+                .disable();
+    
+            return http.build();
+        }
 
 
 
