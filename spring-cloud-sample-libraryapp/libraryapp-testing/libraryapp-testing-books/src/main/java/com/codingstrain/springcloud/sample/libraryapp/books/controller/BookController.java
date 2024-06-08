64257150c72ce834631b2d236d80351603eeff75
macasari <macasari@MACASARIW.mshome.net>
2024-06-08 23:04:56 +0200

    add testing sample

---
+++ spring-cloud-sample-libraryapp/libraryapp-testing/libraryapp-testing-books/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/controller/BookController.java
@@ -0,0 +1,35 @@
+package com.codingstrain.springcloud.sample.libraryapp.books.controller;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codingstrain.springcloud.sample.libraryapp.books.service.BookService;
+import com.codingstrain.springcloud.sample.libraryapp.model.entity.Author;
+
+@RestController
+@RequestMapping("/library")
+public class BookController {
+
+    Logger logger = LoggerFactory.getLogger(BookService.class);
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping(value = "/authorInfo", params = { "authorName" })
+    public Optional<Author> getAuthor(@RequestParam("authorName") String authorName) {
+        return bookService.getAuthor(authorName);
+    }
+
+    @GetMapping(value = "/ping")
+    public String ping() {
+        return "fffff";
+    }
+
+}
