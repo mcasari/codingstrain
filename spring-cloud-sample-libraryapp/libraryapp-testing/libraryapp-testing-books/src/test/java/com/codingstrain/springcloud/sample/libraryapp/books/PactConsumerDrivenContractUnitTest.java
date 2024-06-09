347355bd34208bd90a6576290ac706dfb436d7b2
macasari <macasari@MACASARIW.mshome.net>
2024-06-09 23:59:58 +0200

    advancement

--- spring-cloud-sample-libraryapp/libraryapp-testing/libraryapp-testing-books/src/test/java/com/codingstrain/springcloud/sample/libraryapp/books/PactConsumerDrivenContractUnitTest.java
+++ spring-cloud-sample-libraryapp/libraryapp-testing/libraryapp-testing-books/src/test/java/com/codingstrain/springcloud/sample/libraryapp/books/PactConsumerDrivenContractUnitTest.java
@@ -1,4 +1,4 @@
-package com.codingstrain.pact;
+package com.codingstrain.springcloud.sample.libraryapp.books;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,14 +13,14 @@ import au.com.dius.pact.consumer.MockServer;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 import au.com.dius.pact.consumer.junit5.PactTestFor;
-import au.com.dius.pact.core.model.RequestResponsePact;
+import au.com.dius.pact.core.model.V4Pact;
 import au.com.dius.pact.core.model.annotations.Pact;
 
 @ExtendWith(PactConsumerTestExt.class)
 public class PactConsumerDrivenContractUnitTest {
 
     @Pact(consumer = "ConsumerService", provider = "ProviderService")
-    public RequestResponsePact createPact(PactDslWithProvider builder) {
+    public V4Pact createPact(PactDslWithProvider builder) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
 
@@ -34,7 +34,7 @@ public class PactConsumerDrivenContractUnitTest {
             .status(200)
             .headers(headers)
             .body("{\"name\":\"Goethe\",\"biography\":\"Bio info\"}")
-          .toPact();
+            .toPact(V4Pact.class);
     }
 
     @Test
