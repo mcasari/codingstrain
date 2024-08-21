811e53796abd524d180e938e2265c26369a8c49d
mcasari <mcasari@host.docker.internal>
2024-08-21 21:07:36 +0200

    fix configuration

--- spring-cloud-sample-libraryapp/libraryapp-discovery-ssl/libraryapp-discovery-books-ssl/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/AppMain.java
+++ spring-cloud-sample-libraryapp/libraryapp-discovery-ssl/libraryapp-discovery-books-ssl/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/AppMain.java
@@ -20,14 +20,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableFeignClients
 @RefreshScope
 public class AppMain {
 
