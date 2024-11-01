cff6fa0aff03b44d2c06a581fce29750c5da98e0
macasari <macasari@MACASARIW.italy.itroot.adnet>
2024-11-01 19:01:52 +0100

    advancement

--- spring-cloud-sample-libraryapp/libraryapp-messagedriven/libraryapp-messagedriven-booksmanagement/src/main/java/com/codingstrain/springcloud/sample/libraryapp/booksmanagement/controller/BooksManagementController.java
+++ spring-cloud-sample-libraryapp/libraryapp-messagedriven/libraryapp-messagedriven-booksmanagement/src/main/java/com/codingstrain/springcloud/sample/libraryapp/booksmanagement/controller/BooksManagementController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codingstrain.springcloud.sample.libraryapp.booksmanagement.dto.Author;
+import com.codingstrain.springcloud.sample.libraryapp.booksmanagement.dto.Book;
+import com.codingstrain.springcloud.sample.libraryapp.booksmanagement.dto.BookInfo;
+
 @RestController
 @RequestMapping("/booksmanagement")
 public class BooksManagementController {
@@ -16,35 +20,17 @@ public class BooksManagementController {
     @GetMapping(value = "/sendBookInfo")
     public void newBookItem() {
         BookInfo bookInfo = new BookInfo();
+        Book book = new Book();
+        book.setTitle("The Naked Sun");
+        book.setGenre("Sci-fi");
+        Author author = new Author();
+        author.setName("Isaac Asimov");
+        author.setBiographyInfo("Biography Info");
+
         System.out.println("Sending " + bookInfo);
         streamBridge.send("sendBookInfo-out-0", bookInfo);
     }
 
-    class BookInfo {
-        String bookTitle;
-        String author;
 
-        public String getAuthor() {
-            return author;
-        }
-
-        public String getBookTitle() {
-            return bookTitle;
-        }
-
-        public void setAuthor(String author) {
-            this.author = author;
-        }
-
-        public void setBookTitle(String bookTitle) {
-            this.bookTitle = bookTitle;
-        }
-
-        @Override
-        public String toString() {
-            return "BookInfo [bookTitle=" + bookTitle + ", author=" + author + "]";
-        }
-
-    }
 
 }
