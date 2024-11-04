a96ffe10bfcf52e7b84903f8a2622b1e61f50795
macasari <macasari@MACASARIW.italy.itroot.adnet>
2024-11-04 23:14:16 +0100

    advancement

--- spring-cloud-sample-libraryapp/libraryapp-messagedriven/libraryapp-messagedriven-authors/src/main/java/com/codingstrain/springcloud/sample/libraryapp/authors/AppMain.java
+++ spring-cloud-sample-libraryapp/libraryapp-messagedriven/libraryapp-messagedriven-authors/src/main/java/com/codingstrain/springcloud/sample/libraryapp/authors/AppMain.java
@@ -1,15 +1,18 @@
 package com.codingstrain.springcloud.sample.libraryapp.authors;
 
 import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppMain {
+
+    @Autowired
+    private ServerProperties serverProperties;
 
 	public static void main(String[] args) {
         new SpringApplicationBuilder(AppMain.class).run(args);
@@ -19,24 +22,7 @@ public class AppMain {
     public Consumer<String> receive() {
         return payload -> {
             System.out.println(payload);
-        };
-    }
-
-    @Bean
-    public Supplier<String> source() {
-        return () -> {
-            String message = "Supplier message";
-            System.out.println("Sending value: " + message);
-            return message;
-
-        };
-    }
-
-    @Bean
-    public Function<String, String> uppercase() {
-        return payload -> {
-            System.out.println(payload);
-            return payload.toUpperCase();
+            System.out.println("SERVER PORT: " + serverProperties.getPort());
         };
     }
 
--- spring-cloud-sample-libraryapp/libraryapp-messagedriven/libraryapp-messagedriven-books/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/AppMain.java
+++ spring-cloud-sample-libraryapp/libraryapp-messagedriven/libraryapp-messagedriven-books/src/main/java/com/codingstrain/springcloud/sample/libraryapp/books/AppMain.java
@@ -2,12 +2,17 @@ package com.codingstrain.springcloud.sample.libraryapp.books;
 
 import java.util.function.Consumer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AppMain {
+
+    @Autowired
+    private ServerProperties serverProperties;
 
 	public static void main(String[] args) {
         new SpringApplicationBuilder(AppMain.class).run(args);
@@ -17,6 +22,7 @@ public class AppMain {
     public Consumer<String> receive() {
         return payload -> {
             System.out.println(payload);
+            System.out.println("SERVER PORT: " + serverProperties.getPort());
         };
     }
 
