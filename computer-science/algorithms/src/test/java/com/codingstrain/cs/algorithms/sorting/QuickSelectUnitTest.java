a7336726b2329296eb44ec9ebdf408384a223946
Mario Angelo Casari <mario.casari@gmail.com>
2025-03-27 14:50:43 +0100

    quickselect unittest

--- computer-science/algorithms/src/test/java/com/codingstrain/cs/algorithms/sorting/QuickSelectUnitTest.java
+++ computer-science/algorithms/src/test/java/com/codingstrain/cs/algorithms/sorting/QuickSelectUnitTest.java
@@ -8,26 +8,26 @@ class QuickSelectUnitTest {
 	public void quickSelectTest() throws Exception {
 		int[] array = { 8, 9, 4, 3, 23, 15 };
 		int n = 3;
-		System.out.println(quickSelect(array, 0, array.length - 1, n - 1));
+		System.out.println(quickSel(array, 0, array.length - 1, n - 1));
 	}
 
-    private static int quickSelect(int[] array, int l, int r, int n) {
+    private static int quickSel(int[] array, int l, int r, int n) {
         if (l == r) {
             return array[l];
         }
 
-        int pivot = partition(array, l, r);
+        int pivot = part(array, l, r);
         
         if (pivot == n) {
             return array[pivot];
         } else if (pivot > n) {
-            return quickSelect(array, l, pivot - 1, n);
+            return quickSel(array, l, pivot - 1, n);
         } else {
-            return quickSelect(array, pivot + 1, r, n);
+            return quickSel(array, pivot + 1, r, n);
         }
     }
 
-    private static int partition(int[] array, int l, int r) {
+    private static int part(int[] array, int l, int r) {
         int pivot = array[r];
         int i = l;
         for (int j = l; j < r; j++) {
@@ -40,10 +40,10 @@ class QuickSelectUnitTest {
         return i;
     }
 
-    private static void swapElements(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    private static void swapElements(int[] array, int i, int j) {
+        int tempelem = array[i];
+        array[i] = array[j];
+        array[j] = tempelem;
     }
 
 }
