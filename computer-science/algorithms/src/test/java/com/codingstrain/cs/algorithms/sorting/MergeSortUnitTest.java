5a2c5712f5ab81c4a4cf1233d91ffab1babb14f1
macasari <mario.casari@gmail.com>
2025-03-28 22:33:25 +0100

    refactoring

--- computer-science/algorithms/src/test/java/com/codingstrain/cs/algorithms/sorting/MergeSortUnitTest.java
+++ computer-science/algorithms/src/test/java/com/codingstrain/cs/algorithms/sorting/MergeSortUnitTest.java
@@ -13,63 +13,51 @@ class MergeSortUnitTest {
         }
 	}
 
-    // Function to perform merge sort
-    public static void mergeSort(int[] array, int left, int right) {
+    private void mergeSort(int[] arrayToSort, int left, int right) {
         if (left < right) {
             int mid = left + (right - left) / 2;
+           
+            mergeSort(arrayToSort, left, mid);
+            mergeSort(arrayToSort, mid + 1, right);
             
-            // Recursively sort first and second halves
-            mergeSort(array, left, mid);
-            mergeSort(array, mid + 1, right);
-            
-            // Merge the sorted halves
-            merge(array, left, mid, right);
+            merge(arrayToSort, left, mid, right);
         }
     }
     
-    // Function to merge two subarrays
-    public static void merge(int[] array, int left, int mid, int right) {
+    private void merge(int[] arrayToMerge, int left, int mid, int right) {
         int n1 = mid - left + 1;
         int n2 = right - mid;
         
         int[] leftArray = new int[n1];
         int[] rightArray = new int[n2];
         
-        // Copy data to temporary arrays
         for (int i = 0; i < n1; i++)
-            leftArray[i] = array[left + i];
+            leftArray[i] = arrayToMerge[left + i];
         for (int j = 0; j < n2; j++)
-            rightArray[j] = array[mid + 1 + j];
+            rightArray[j] = arrayToMerge[mid + 1 + j];
         
         int i = 0, j = 0, k = left;
-        
-        // Merge the temporary arrays back into the main array
         while (i < n1 && j < n2) {
             if (leftArray[i] <= rightArray[j]) {
-                array[k] = leftArray[i];
+            	arrayToMerge[k] = leftArray[i];
                 i++;
             } else {
-                array[k] = rightArray[j];
+            	arrayToMerge[k] = rightArray[j];
                 j++;
             }
             k++;
         }
         
-        // Copy any remaining elements of leftArray
         while (i < n1) {
-            array[k] = leftArray[i];
+        	arrayToMerge[k] = leftArray[i];
             i++;
             k++;
         }
         
-        // Copy any remaining elements of rightArray
         while (j < n2) {
-            array[k] = rightArray[j];
+        	arrayToMerge[k] = rightArray[j];
             j++;
             k++;
         }
     }
-
-
-
 }
