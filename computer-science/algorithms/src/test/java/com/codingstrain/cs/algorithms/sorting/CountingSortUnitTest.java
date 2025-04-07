84965ef647696e46ae4e0ae725b90b0b304365a2
macasari <mario.casari@gmail.com>
2025-04-07 20:41:35 +0200

    format

--- computer-science/algorithms/src/test/java/com/codingstrain/cs/algorithms/sorting/CountingSortUnitTest.java
+++ computer-science/algorithms/src/test/java/com/codingstrain/cs/algorithms/sorting/CountingSortUnitTest.java
@@ -8,32 +8,32 @@ class CountingSortUnitTest {
 
 	@Test
 	public void HeapSortTest() throws Exception {
-        int[] arrToBeSorted = {13, 15, 21, 57, 67, 71};   
-        cSort(arrToBeSorted);
-        for (int elem : arrToBeSorted) {
-            System.out.println(elem + " ");
-        }
+		int[] arrToBeSorted = { 13, 15, 21, 57, 67, 71 };
+		cSort(arrToBeSorted);
+		for (int elem : arrToBeSorted) {
+			System.out.println(elem + " ");
+		}
 	}
-	
-    public static void cSort(int[] arrToBeSorted) {
-        if (arrToBeSorted.length == 0) return;
-        int max = Arrays.stream(arrToBeSorted).max().getAsInt();
-        int[] countArray = new int[max + 1];
 
-        for (int num : arrToBeSorted) {
-        	countArray[num]++;
-        }
+	public static void cSort(int[] arrToBeSorted) {
+		if (arrToBeSorted.length == 0)
+			return;
+		int max = Arrays.stream(arrToBeSorted).max().getAsInt();
+		int[] countArray = new int[max + 1];
 
-        for (int i = 1; i < countArray.length; i++) {
-        	countArray[i] += countArray[i - 1];
-        }
+		for (int num : arrToBeSorted) {
+			countArray[num]++;
+		}
 
-        int[] output = new int[arrToBeSorted.length];
-        for (int i = arrToBeSorted.length - 1; i >= 0; i--) {
-            output[--countArray[arrToBeSorted[i]]] = arrToBeSorted[i];
-        }
-        System.arraycopy(output, 0, arrToBeSorted, 0, arrToBeSorted.length);
-    }
+		for (int i = 1; i < countArray.length; i++) {
+			countArray[i] += countArray[i - 1];
+		}
 
-    
+		int[] output = new int[arrToBeSorted.length];
+		for (int i = arrToBeSorted.length - 1; i >= 0; i--) {
+			output[--countArray[arrToBeSorted[i]]] = arrToBeSorted[i];
+		}
+		System.arraycopy(output, 0, arrToBeSorted, 0, arrToBeSorted.length);
+	}
+
 }
