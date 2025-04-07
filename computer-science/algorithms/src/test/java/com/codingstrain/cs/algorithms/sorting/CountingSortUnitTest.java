c5723cc61bafadc9c9dbf9388e496ce8c8f5bece
macasari <mario.casari@gmail.com>
2025-04-07 20:39:09 +0200

    implement counting sort

---
+++ computer-science/algorithms/src/test/java/com/codingstrain/cs/algorithms/sorting/CountingSortUnitTest.java
@@ -0,0 +1,39 @@
+package com.codingstrain.cs.algorithms.sorting;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+class CountingSortUnitTest {
+
+	@Test
+	public void HeapSortTest() throws Exception {
+        int[] arrToBeSorted = {13, 15, 21, 57, 67, 71};   
+        cSort(arrToBeSorted);
+        for (int elem : arrToBeSorted) {
+            System.out.println(elem + " ");
+        }
+	}
+	
+    public static void cSort(int[] arrToBeSorted) {
+        if (arrToBeSorted.length == 0) return;
+        int max = Arrays.stream(arrToBeSorted).max().getAsInt();
+        int[] countArray = new int[max + 1];
+
+        for (int num : arrToBeSorted) {
+        	countArray[num]++;
+        }
+
+        for (int i = 1; i < countArray.length; i++) {
+        	countArray[i] += countArray[i - 1];
+        }
+
+        int[] output = new int[arrToBeSorted.length];
+        for (int i = arrToBeSorted.length - 1; i >= 0; i--) {
+            output[--countArray[arrToBeSorted[i]]] = arrToBeSorted[i];
+        }
+        System.arraycopy(output, 0, arrToBeSorted, 0, arrToBeSorted.length);
+    }
+
+    
+}
