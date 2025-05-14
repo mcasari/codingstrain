0b2bf1d35a1e899eef9991f60f0e27d88e1211e9
macasari <mario.casari@gmail.com>
2025-05-14 21:52:02 +0200

    Merge branch 'main' of https://github.com/mcasari/codingstrain.git into main

--- computer-science/algorithms/src/test/java/com/codingstrain/cs/algorithms/trees/CheckBinaryTreeBST.java
+++ computer-science/algorithms/src/test/java/com/codingstrain/cs/algorithms/trees/CheckBinaryTreeBST.java
@@ -1,58 +1,43 @@
 package com.codingstrain.cs.algorithms.trees;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class CheckBinaryTreeBST {
 
-    public static void printByLayer(BinaryNode startNode) {
-        if (startNode == null) return;
+	class TreeNode {
+	    int val;
+	    TreeNode left, right;
+	    TreeNode(int val) {
+	        this.val = val;
+	    }
+	}
 
-        Queue<BinaryNode> pendingNodes = new LinkedList<>();
-        pendingNodes.add(startNode);
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
 
-        while (!pendingNodes.isEmpty()) {
-            BinaryNode active = pendingNodes.poll();
-            System.out.print(active.key + " ");
-
-            if (active.leftBranch != null) {
-                pendingNodes.add(active.leftBranch);
-            }
-
-            if (active.rightBranch != null) {
-                pendingNodes.add(active.rightBranch);
-            }
-        }
+    private boolean isValidBST(TreeNode node, long min, long max) {
+        if (node == null) return true;
+        if (node.val <= min || node.val >= max) return false;
+        return isValidBST(node.left, min, node.val) &&
+               isValidBST(node.right, node.val, max);
     }
 
     public static void main(String[] args) {
         /*
-                8
-               / \
-              4   12
-             / \    \
-            2   6    14
+              10
+             /  \
+            5    15
+                /  \
+               12   20
         */
+        TreeNode root = new CheckBinaryTreeBST().new TreeNode(10);
+        root.left = new CheckBinaryTreeBST().new TreeNode(5);
+        root.right = new CheckBinaryTreeBST().new TreeNode(15);
+        root.right.left = new CheckBinaryTreeBST().new TreeNode(12);
+        root.right.right = new CheckBinaryTreeBST().new TreeNode(20);
 
-        BinaryNode treeRoot = new CheckBinaryTreeBST().new BinaryNode(8);
-        treeRoot.leftBranch = new CheckBinaryTreeBST().new BinaryNode(4);
-        treeRoot.rightBranch = new CheckBinaryTreeBST().new BinaryNode(12);
-        treeRoot.leftBranch.leftBranch = new CheckBinaryTreeBST().new BinaryNode(2);
-        treeRoot.leftBranch.rightBranch = new CheckBinaryTreeBST().new BinaryNode(6);
-        treeRoot.rightBranch.rightBranch = new CheckBinaryTreeBST().new BinaryNode(14);
-
-        System.out.print("Level Order Output: ");
-        printByLayer(treeRoot);
-    }
-    
-    class BinaryNode {
-        int key;
-        BinaryNode leftBranch;
-        BinaryNode rightBranch;
-
-        BinaryNode(int key) {
-            this.key = key;
-        }
+        CheckBinaryTreeBST validator = new CheckBinaryTreeBST();
+        boolean result = validator.isValidBST(root);
+        System.out.println("Is the tree a valid BST? " + result);
     }
 }
 
