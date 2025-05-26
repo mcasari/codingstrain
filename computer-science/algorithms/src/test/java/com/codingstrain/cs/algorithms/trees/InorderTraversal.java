109c697f782af00a4b7ec88d015dfd44b4a1762a
macasari <mario.casari@gmail.com>
2025-05-26 21:42:43 +0200

    refactoring

--- computer-science/algorithms/src/test/java/com/codingstrain/cs/algorithms/trees/InorderTraversal.java
+++ computer-science/algorithms/src/test/java/com/codingstrain/cs/algorithms/trees/InorderTraversal.java
@@ -4,35 +4,35 @@ import java.util.Stack;
 
 public class InorderTraversal {
 	
-	class TreeNode {
+	class TreeNodeStructure {
 	    int value;
-	    TreeNode left;
-	    TreeNode right;
+	    TreeNodeStructure left;
+	    TreeNodeStructure right;
 
-	    TreeNode(int value) {
+	    TreeNodeStructure(int value) {
 	        this.value = value;
 	        this.left = null;
 	        this.right = null;
 	    }
 	}
 
-    public static void inorderIterative(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode current = root;
+    public static void inorderIterative(TreeNodeStructure root) {
+        Stack<TreeNodeStructure> stack = new Stack<>();
+        TreeNodeStructure currentNode = root;
 
-        while (current != null || !stack.isEmpty()) {
+        while (currentNode != null || !stack.isEmpty()) {
             // Reach the leftmost node of the current node
-            while (current != null) {
-                stack.push(current);
-                current = current.left;
+            while (currentNode != null) {
+                stack.push(currentNode);
+                currentNode = currentNode.left;
             }
 
             // Current must be null at this point
-            current = stack.pop();
-            System.out.print(current.value + " ");
+            currentNode = stack.pop();
+            System.out.print(currentNode.value + " ");
             
             // Visit the right subtree
-            current = current.right;
+            currentNode = currentNode.right;
         }
     }
 
@@ -45,11 +45,11 @@ public class InorderTraversal {
                  / \
                 1   3
         */
-        TreeNode root = new InorderTraversal().new TreeNode(4);
-        root.left = new InorderTraversal().new TreeNode(2);
-        root.right = new InorderTraversal().new TreeNode(5);
-        root.left.left = new InorderTraversal().new TreeNode(1);
-        root.left.right = new InorderTraversal().new TreeNode(3);
+        TreeNodeStructure root = new InorderTraversal().new TreeNodeStructure(4);
+        root.left = new InorderTraversal().new TreeNodeStructure(2);
+        root.right = new InorderTraversal().new TreeNodeStructure(5);
+        root.left.left = new InorderTraversal().new TreeNodeStructure(1);
+        root.left.right = new InorderTraversal().new TreeNodeStructure(3);
 
         System.out.println("Inorder traversal (non-recursive):");
         inorderIterative(root);
