1179a398d055ac3b5f8ffd61a8db102f08073294
mcasari <mario.casari@gmail.com>
2025-11-25 00:30:40 +0100

    refactoring

--- computer-science/algorithms/src/test/java/com/codingstrain/cs/datastructures/Tree.java
+++ computer-science/algorithms/src/test/java/com/codingstrain/cs/datastructures/Tree.java
@@ -72,8 +72,6 @@ public class Tree {
         Tree binaryTree = new Tree();
         binaryTree.add(10);
         binaryTree.add(5);
-        
-        
         binaryTree.add(15);
         binaryTree.add(3);
         binaryTree.add(7);
