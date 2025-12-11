a30cf278d73cb4786d4326606185aca26bf69783
mcasari <mario.casari@gmail.com>
2025-12-11 23:09:35 +0100

    refac

--- computer-science/algorithms/src/test/java/com/codingstrain/tweetexamples/Example2.java
+++ computer-science/algorithms/src/test/java/com/codingstrain/tweetexamples/Example2.java
@@ -73,6 +73,7 @@ public class Example2 {
         binaryTree.add(10);
         binaryTree.add(11);
         binaryTree.add(12);
+        binaryTree.add(13);
         //
         System.out.println("In-order traversal:");
         binaryTree.traverseInOrder(); // Expected output: 3 5 7 10 15
