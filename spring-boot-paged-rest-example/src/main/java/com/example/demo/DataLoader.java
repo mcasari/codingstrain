ef342f2fb13f279d40f537473c58dbc4226bd88d
'mcasari' <mario.casari@gmail.com>
2026-01-03 23:16:07 +0100

    refac

--- spring-boot-paged-rest-example/src/main/java/com/example/demo/DataLoader.java
+++ spring-boot-paged-rest-example/src/main/java/com/example/demo/DataLoader.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DataLoader {
 
+	
     @Bean
     CommandLineRunner loadData(UserRepository repository) {
         return args -> {
