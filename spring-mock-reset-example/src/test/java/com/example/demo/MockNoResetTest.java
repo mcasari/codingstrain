e60b00eea8d00b5aa0cdee551148604bdfba873b
'mcasari' <mario.casari@gmail.com>
2026-04-03 23:06:47 +0200

    no reset test

---
+++ spring-mock-reset-example/src/test/java/com/example/demo/MockNoResetTest.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockReset;
+
+@SpringBootTest
+class MockNoResetTest {
+
+
+    @Autowired
+    private MyController controller;
+
+    @MockBean(reset = MockReset.NONE)
+    private MyService myService;
+
+    @Test
+    @Order(1)
+    void testA() {
+        when(myService.getData()).thenReturn("A");
+        System.out.println(controller.fetch()); // A
+    }
+
+    @Test
+    @Order(2)
+    void testB() {
+        System.out.println(controller.fetch()); // should be A if leakage happens
+    }
+}
